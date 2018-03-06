2279e18b1fd0bddaf989a63400a023405a37d32d
Fabien Ferrero <32847282+FabienFerrero@users.noreply.github.com>
2018-03-06 23:48:35 +0100

    Add files via upload

---
+++ Arduino_Code/LoRa_PHY/LoRa_test_States.ino
@@ -0,0 +1,170 @@
+/*
+  LoRa test States
+
+  The objective of this sketch is to measure the power consumption in different states
+
+  The code was modified from LoRa Duplex from  Tom Igoe
+*/
+#include <SPI.h>              // include libraries
+#include <LoRa.h>
+#include "LowPower.h"
+
+#define debugSerial Serial
+#define SHOW_DEBUGINFO
+
+
+int txPower = 14; // Will affect the power consumption in Transmit mode
+
+
+int spreadingFactor = 12; // from 7 to 12
+long signalBandwidth = 125E3; // 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3,41.7E3,62.5E3,125E3,250E3
+int codingRateDenominator=5;
+int preambleLength=8;
+
+int receive = 0;
+
+const int csPin = 10;          // LoRa radio chip select
+const int resetPin = 8;       // LoRa radio reset
+const int irqPin = 2;         // change for your board; must be a hardware interrupt pin
+
+String outgoing;              // outgoing message
+
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xFF;      // destination to send to
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // interval between sends
+
+void setup() {
+  
+  delay(2000); // wait to be sure that measurement with a DC source will start
+  Serial.begin(9600);                   // initialize serial
+  while (!Serial);
+  Serial.println("LoRa States power Consumption");
+
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
+  if (!LoRa.begin(870E6)) {             // initialize ratio at 870 MHz to limit interferance with LoRaWan
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
+  }
+
+  Serial.println("LoRa init succeeded.");
+  LoRa.setTxPower(txPower,1);
+  LoRa.setSpreadingFactor(spreadingFactor);
+  LoRa.setSignalBandwidth(signalBandwidth);
+  LoRa.setCodingRate4(codingRateDenominator);
+  LoRa.setPreambleLength(preambleLength);
+}
+
+void loop() {
+  if (millis() - lastSendTime > interval) {
+    String message = "HeLoRa World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";   // send a message
+    Serial.println("Sending State : " + message);
+    sendMessage(message);
+    
+    lastSendTime = millis();            // timestamp the message
+    interval = 10000;    // 2-3 seconds
+    LoRa.sleep(); // move LoRa transciever in Sleep mode
+    do_sleep(5);
+    receive = 1;
+  }
+
+  // parse for a packet, and call onReceive with the result:
+  if (receive == 1)
+  {
+  Serial.println("Receiving State ");
+  receive = 0;}
+  onReceive(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+}
+
+extern volatile unsigned long timer0_millis;
+void addMillis(unsigned long extra_millis) {
+  uint8_t oldSREG = SREG;
+  cli();
+  timer0_millis += extra_millis;
+  SREG = oldSREG;
+  sei();
+}
+
+void do_sleep(unsigned int sleepyTime) {
+  unsigned int eights = sleepyTime / 8;
+  unsigned int fours = (sleepyTime % 8) / 4;
+  unsigned int twos = ((sleepyTime % 8) % 4) / 2;
+  unsigned int ones = ((sleepyTime % 8) % 4) % 2;
+
+#ifdef SHOW_DEBUGINFO
+  Serial.print("Sleeping for ");
+  Serial.print(sleepyTime);
+  Serial.println(" seconds");  
+  delay(100); //Wait for serial to complete
+#endif
+
+
+  for ( int x = 0; x < eights; x++) {
+    // put the processor to sleep for 8 seconds
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+  for ( int x = 0; x < fours; x++) {
+    // put the processor to sleep for 4 seconds
+    LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+  }
+  for ( int x = 0; x < twos; x++) {
+    // put the processor to sleep for 2 seconds
+    LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
+  }
+  for ( int x = 0; x < ones; x++) {
+    // put the processor to sleep for 1 seconds
+    LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+  }
+  addMillis(sleepyTime * 1000);
+}
+
