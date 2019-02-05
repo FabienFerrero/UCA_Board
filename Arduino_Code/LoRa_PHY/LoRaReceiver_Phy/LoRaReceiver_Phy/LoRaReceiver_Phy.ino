9c6e56a97ba5c3703a8ad2d743052aa3d842c2fb
Fabien Ferrero <32847282+FabienFerrero@users.noreply.github.com>
2019-02-05 19:41:47 +0100

    update LoRa Phy

---
+++ Arduino_Code/LoRa_PHY/LoRaReceiver_Phy/LoRaReceiver_Phy/LoRaReceiver_Phy.ino
@@ -0,0 +1,70 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+// Parameters you can play with :
+
+int txPower = 14; // from 0 to 20, default is 14
+int spreadingFactor = 12; // from 7 to 12, default is 12
+long signalBandwidth = 125E3; // 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3,41.7E3,62.5E3,125E3,250E3,500e3, default is 125E3
+int codingRateDenominator=5; // Numerator is 4, and denominator from 5 to 8, default is 5
+int preambleLength=8; // from 2 to 20, default is 8
+String payload = "hello"; // you can change the payload
+
+#define SS 10
+#define RST 8
+#define DI0 2
+#define BAND 865E6  // Here you define the frequency carrier
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+  Serial.print("SetFrequency : ");
+  Serial.print(BAND);
+  Serial.println("Hz");
+  Serial.print("SetSpreadingFactor : SF");
+  Serial.println(spreadingFactor);
+  
+
+  SPI.begin();
+  LoRa.setPins(SS,RST,DI0);
+
+  
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+ LoRa.setTxPower(txPower,1);
+ LoRa.setSpreadingFactor(spreadingFactor);
+ LoRa.setSignalBandwidth(signalBandwidth);
+ LoRa.setCodingRate4(codingRateDenominator);
+ LoRa.setPreambleLength(preambleLength);
+ 
+}
+
+void loop() {
+   // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
+
+
+
+
+ 
---
+++ Arduino_Code/LoRa_PHY/LoRaSender_Phy/LoRaSender_Phy.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+// Parameters you can play with :
+
+int txPower = 14; // from 0 to 20, default is 14
+int spreadingFactor = 12; // from 7 to 12, default is 12
+long signalBandwidth = 125E3; // 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3,41.7E3,62.5E3,125E3,250E3,500e3, default is 125E3
+int codingRateDenominator=5; // Numerator is 4, and denominator from 5 to 8, default is 5
+int preambleLength=8; // from 2 to 20, default is 8
+String payload = "hello"; // you can change the payload
+
+#define SS 10
+#define RST 8
+#define DI0 2
+#define BAND 865E6  // Here you define the frequency carrier
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+  Serial.print("SetFrequency : ");
+  Serial.print(BAND);
+  Serial.println("Hz");
+  Serial.print("SetSpreadingFactor : SF");
+  Serial.println(spreadingFactor);
+
+  SPI.begin();
+  LoRa.setPins(SS,RST,DI0);
+
+  
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+ LoRa.setTxPower(txPower,1);
+ LoRa.setSpreadingFactor(spreadingFactor);
+ LoRa.setSignalBandwidth(signalBandwidth);
+ LoRa.setCodingRate4(codingRateDenominator);
+ LoRa.setPreambleLength(preambleLength);
+// LoRa.setPolarity(1);
+ //LoRa.setFSK(); 
+ 
+}
+
+void loop() {
+  
+  // send packet
+  
+  LoRa.beginPacket();
+  LoRa.print(payload);  
+  
+  LoRa.endPacket();
+  counter++;
+
+  Serial.print("Sending packet with payload {");
+  Serial.print(payload);
+  Serial.print("} N°");
+  Serial.println(counter);
+ 
+
+  delay(100);
+}
